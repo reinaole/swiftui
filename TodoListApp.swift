5600304afd8fe272fff25dc84424134c1c71fb4b
Ole Ejby Reinau <oleejby@me.com>
2023-01-24 10:00:15 +0100

    Add files via upload

---
+++ TodoListApp.swift
@@ -0,0 +1,36 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Nick Sarno on 3/2/21.
+//
+
+import SwiftUI
+
+/*
+ MVVM Architecture
+ 
+ Model - manages single data point
+ View - manages the UI
+ ViewModel - manages data (models) for views
+ */
+
+@main
+struct TodoListApp: App
+{
+    
+    @StateObject var listViewModel: ListViewModel = ListViewModel()
+    
+    var body: some Scene
+    {
+        WindowGroup
+        {
+            NavigationView
+            {
+                ListView()
+            }
+            .navigationViewStyle(StackNavigationViewStyle())
+            .environmentObject(listViewModel)
+        }
+    }
+}
